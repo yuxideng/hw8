2ad8f429f8b7c0660a1ebbcb781d0d65fc839933
yuxideng <penny.deng7@gmail.com>
2016-11-29 18:31:54 -0600

    Update Grin.java

--- src/Grin.java
+++ src/Grin.java
@@ -1,12 +1,14 @@
+import java.io.IOException;
+
 public class Grin {
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		String command = args[0];
 		String in = args[1];
 		String out = args[2];
 		if(command.equals("encode")){
-			GrinEncode(in,out);
+			GrinEncoder.encode(in,out);
 		}else if (command.equals("decode")){
-			GrinDecode(in,out);
+			GrinDecoder.decode(in,out);
 		}else{
 			System.out.printf("Enter either encode or decode");
 		}
